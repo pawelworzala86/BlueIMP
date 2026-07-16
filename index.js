c216af566c9b477de15feceb158c243c65115e76
Paweł Worzała <pawelworzala86@gmail.com>
2026-07-16 15:32:36 +0200

    numbers declarations

--- index.js
+++ index.js
@@ -2,11 +2,87 @@ const fs = require('fs')
 
 let source = fs.readFileSync('./examples/test.bi').toString()
 
-let lines = source.split('/n').map(l=>l.trim())
+let lines = source.split('\n').map(l=>l.trim())
+
+function toHex(num, length) {
+    return num.toString(16).toUpperCase().padStart(length*2, "0")
+}
+function floatToHex32(value) {
+    const buffer = new ArrayBuffer(4)
+    const view = new DataView(buffer)
+    view.setFloat32(0, value, true) // little-endian
+    let hex = ""
+    for (let i = 0; i < 4; i++) {
+        hex += view.getUint8(i).toString(16).padStart(2, "0").toUpperCase()
+    }
+    return hex
+}
+function floatToHex64(value) {
+    const buffer = new ArrayBuffer(8)
+    const view = new DataView(buffer)
+    view.setFloat64(0, value, true) // little-endian
+    let hex = ""
+    for (let i = 0; i < 8; i++) {
+        hex += view.getUint8(i).toString(16).padStart(2, "0").toUpperCase()
+    }
+    return hex
+}
+function intToHexSigned(value, bytes) {
+    const max = 2 ** (bytes * 8)
+    let unsigned = value
+    // konwersja ujemnych na two's complement
+    if (value < 0) {
+        unsigned = max + value
+    }
+    return unsigned
+        .toString(16)
+        .toUpperCase()
+        .padStart(bytes * 2, "0")
+}
+function numToHex(num,bytes=8){
+    if((num.indexOf('f')>-1)||(num.indexOf('.')>-1)){
+        num = num.replace('f','')
+        if(bytes==4){
+            return floatToHex32(Number(num))
+        }
+        return floatToHex64(Number(num))
+    }else if(num.indexOf('i')>-1){
+        num = num.replace('i','')
+        return intToHexSigned(Number(num),bytes)
+    }else{
+        num = num.replace('u','')
+        return toHex(Number(num),bytes)
+    }
+}
+
+function formatHex(hex) {
+    return hex
+        .replace(/[^0-9A-Fa-f]/g, "")   // usuń śmieci
+        .match(/.{1,2}/g)               // grupy po 2 znaki
+        .join(' ')                      // separator
+        .toUpperCase()
+}
+
 
 lines = lines.map(line=>{
-    const parts = line.trim().split(/\s+/)
-    console.log(parts)
+    const instruction = line.trim().split(/\s+/)[0]
+    let parameters = line.replace(instruction,'').trim().split(',')
+        .map(p=>p.trim())
+    console.log(instruction,parameters)
+
+    if(['db','dd','dq'].includes(instruction)){
+        let bytes = 8
+        if(instruction=='dd'){
+            bytes = 4
+        }else if(instruction=='db'){
+            bytes = 1
+        }
+        parameters = parameters.map(parameter=>{
+            return numToHex(parameter,bytes)
+        })
+        return parameters.map(formatHex).join(' ')
+    }
+
     return line
 })
 
