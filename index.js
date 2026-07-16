8d405f49d890ff863ece816e1fa8ab12d52c8251
Paweł Worzała <pawelworzala86@gmail.com>
2026-07-16 15:09:03 +0200

    split instructions

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ let source = fs.readFileSync('./examples/test.bi').toString()
 
 let lines = source.split('/n').map(l=>l.trim())
 
+lines = lines.map(line=>{
+    const parts = line.trim().split(/\s+/)
+    console.log(parts)
+    return line
+})
+
 source = lines.join('\n')
 
 fs.writeFileSync('./examples/test.txt', source)
