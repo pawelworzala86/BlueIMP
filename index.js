301811cad0165c24d71a8d26759f4caf283fb113
Paweł Worzała <pawelworzala86@gmail.com>
2026-07-16 15:04:00 +0200

    test

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hello world!!!')
+const fs = require('fs')
+
+let source = fs.readFileSync('./examples/test.bi').toString()
+
+let lines = source.split('/n').map(l=>l.trim())
+
+source = lines.join('\n')
+
+fs.writeFileSync('./examples/test.txt', source)
